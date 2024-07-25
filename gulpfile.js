47db32d58a10f9500a7fca5481cce6d4153de190
Karatel <karatel.kara1990@gmail.com>
2024-07-25 15:26:46 +0300

    v. 1.1.1

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import browserSync from 'browser-sync'
 import gulp from 'gulp'
 import autoprefixer from 'gulp-autoprefixer'
 import cleanCSS from 'gulp-clean-css'
+import htmlmin from 'gulp-htmlmin'
 import rename from 'gulp-rename'
 import gulpSass from 'gulp-sass'
 import * as dartSass from 'sass'
@@ -10,7 +11,7 @@ const sass = gulpSass(dartSass)
 gulp.task('server', function () {
 	browserSync({
 		server: {
-			baseDir: 'src',
+			baseDir: 'dist',
 		},
 	})
 
@@ -24,12 +25,48 @@ gulp.task('styles', function () {
 		.pipe(rename({ suffix: '.min', prefix: '' }))
 		.pipe(autoprefixer())
 		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(gulp.dest('src/css'))
+		.pipe(gulp.dest('dist/css'))
 		.pipe(browserSync.stream())
 })
 
 gulp.task('watch', function () {
-	gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'))
+	gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'))
+	gulp.watch('src/*.html').on('change', gulp.parallel('html'))
+})
+gulp.task('html', function () {
+	return gulp
+		.src('src/*.html')
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('dist/'))
+})
+gulp.task('scripts', function () {
+	return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'))
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+gulp.task('fonts', function () {
+	return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'))
+})
+// gulp.task('icons', function () {
+// 	return gulp.src('src/icons/**/*').pipe(gulp.dest('dist/icons'))
+// })
+gulp.task('mailer', function () {
+	return gulp.src('src/mailer/**/*').pipe(gulp.dest('dist/mailer'))
+})
+// gulp.task('images', function () {
+// 	return gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'))
+// })
+
+gulp.task(
+	'default',
+	gulp.parallel(
+		'watch',
+		'server',
+		'styles',
+		'html',
+		'scripts',
+		'fonts',
+		// 'icons',
+		'mailer'
+		// 'images'
+	)
+)
